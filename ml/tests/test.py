90179a8ddde1c2435054c62e229d0c7209241e9a
lelepods <leighalbo23@gmail.com>
2026-07-16 01:16:35 +0800

    updated test.py

--- ml/tests/test.py
+++ ml/tests/test.py
@@ -4,47 +4,51 @@ from scripts.preprocessing import load_dataset
 
 def test_load_dataset():
 
-    df = load_dataset("data/SCOMMERCE_GenZ_UniversityStudent_757_DIB.csv")
+    df = load_dataset("datasets/SCOMMERCE_GenZ_UniversityStudent_757_DIB.csv")
 
     assert isinstance(df, pd.DataFrame)
 
     assert df.shape == (757,31)
 
+from scripts.preprocessing import validate_columns
+
 def test_columns_exist():
 
-    df = load_dataset("data/SCOMMERCE_GenZ_UniversityStudent_757_DIB.csv")
+    df = validate_columns("datasets/SCOMMERCE_GenZ_UniversityStudent_757_DIB.csv")
 
     expected = [
         "Gender",
         "Job",
         "Income",
-        ...
+        "PEU1",
+        "PEU2",
+        "...",
         "AUB4"
     ]
 
     assert list(df.columns) == expected
 
-from scripts.preprocessing import remove_job
+from scripts.preprocessing import drop_columns
 
 def test_remove_job():
 
-    df = load_dataset("data/SCOMMERCE_GenZ_UniversityStudent_757_DIB.csv")
+    df = load_dataset("datasets/SCOMMERCE_GenZ_UniversityStudent_757_DIB.csv")
 
-    df = remove_job(df)
+    df = drop_columns(df, ["Job"])
 
     assert "Job" not in df.columns
 
-from scripts.preprocessing import remove_peu4
+from scripts.preprocessing import drop_columns
 
 def test_remove_peu4():
 
-    df = load_dataset(...)
+    df = load_dataset("datasets/SCOMMERCE_GenZ_UniversityStudent_757_DIB.csv")
 
-    df = remove_peu4(df)
+    df = drop_columns(df, ["PEU4"])
 
     assert "PEU4" not in df.columns
 
-from scripts.preprocessing import compute_composite
+from scripts.preprocessing import compute_composite_score
 
 def test_compute_pu():
 
@@ -57,6 +61,6 @@ def test_compute_pu():
 
     })
 
-    pu = compute_composite(df,["PU1","PU2","PU3","PU4"])
+    pu = compute_composite_score("datasets/SCOMMERCE_GenZ_UniversityStudent_757_DIB.csv",["PU1","PU2","PU3","PU4"], "PU")
 
     assert pu.iloc[0] == 4
