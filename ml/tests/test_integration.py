0b479e776a7f318793002dd0e3aee1af3e9844fb
ulannn14 <rlsrainmackenlhy142005@gmail.com>
2026-08-14 04:21:42 +0800

    fixed import for system testing

--- ml/tests/test_integration.py
+++ ml/tests/test_integration.py
@@ -148,6 +148,15 @@ EXPECTED_UNIQUE_VALUES = [
     [1, 2, 3, 4, 5],
 ]
 
+COMPOSITES = {
+    "PU": ["PU1", "PU2", "PU3", "PU4"],
+    "PEU": ["PEU1", "PEU2", "PEU3"],
+    "FSC": ["FSC1", "FSC2", "FSC3"],
+    "SP": ["SP1", "SP2", "SP3", "SP4"],
+    "TP": ["TP1", "TP2", "TP3"],
+    "IB": ["IB1", "IB2", "IB3", "IB4"],
+    "AUB": ["AUB1", "AUB2", "AUB3", "AUB4"],
+}
 
 # The following integration test checks:
 # - If the complete preprocessing and EDA pipeline reproduces
